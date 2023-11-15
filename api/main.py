13cee63ce083c4b568ad64074e3b5855ca2eb45a
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2023-11-15 14:02:17 -0500

    asdas

--- api/main.py
+++ api/main.py
@@ -2,42 +2,48 @@
 
 import os
 import json
-import redis
-from flask import Flask
-from flask import request
-from linkextractor import extract_links
+from flask import Flask, request, send_file, send_from_directory, jsonify
+from werkzeug.utils import secure_filename
+from plot_image import generate_plot_image
 
 app = Flask(__name__)
-redis_conn = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+
+# Configura la carpeta para cargar archivos
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Función para verificar la extensión permitida del archivo
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
     return "Usage: http://<hostname>[:<prt>]/api/<url>"
 
-@app.route("/api/<path:url>")
-def api(url):
-    qs = request.query_string.decode("utf-8")
-    if qs != "":
-        url += "?" + qs
+@app.route("/api/generate_plot", methods=["POST"])
+def generate_plot():
+    img_path = None
+    if request.method == "POST":
+        file = request.files["filename"]
+        if file and allowed_file(file.filename):
+            # Guarda el archivo CSV en la carpeta 'uploads'
+            filename = secure_filename(file.filename)
+            csv_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(csv_path)
 
-    jsonlinks = redis_conn.get(url)
-    if not jsonlinks:
-        links = extract_links(url)
-        jsonlinks = json.dumps(links, indent=2)
-        redis_conn.set(url, jsonlinks)
+            # Genera la imagen y obtiene la ruta
+            img_path = generate_plot_image(csv_path)
+            print(f"Generated image path: {img_path}")
 
-    response = app.response_class(
-        status=200,
-        mimetype="application/json",
-        response=jsonlinks
-    )
+    # Return JSON response
+    response_data = {"img_path": img_path}
+    return jsonify(response_data)
 
-    return response
+@app.route("/api/img/<filename>")
+def view_image(filename):
+    img_path = os.path.join("img", filename)
+    return send_from_directory("img", filename)
 
-@app.route("/api/generate_plot/<filename>")
-def generate_plot(filename):
-    csv_path = f'data/{filename}'
-    img_path = generate_plot_image(csv_path)
-    return send_file(img_path, mimetype='image/png')
-
-app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
